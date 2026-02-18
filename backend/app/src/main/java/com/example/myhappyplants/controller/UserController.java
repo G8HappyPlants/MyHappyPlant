e6a91b2c29ec9b905e726015b3013e190c8813ba
VenomiZeD <crafterious@gmail.com>
2026-02-18 07:58:29 +0100

    Start work on delete user

--- backend/app/src/main/java/com/example/myhappyplants/controller/UserController.java
+++ backend/app/src/main/java/com/example/myhappyplants/controller/UserController.java
@@ -1,14 +1,41 @@
 package com.example.myhappyplants.controller;
 
+import com.example.myhappyplants.entity.User;
+import com.example.myhappyplants.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-@RestController
+@RestController("/api/user")
 public class UserController {
+	private final UserRepository userRepository;
 
-    @GetMapping("/api/me")
-    public String me(@AuthenticationPrincipal UserDetails userDetails) {
-        return userDetails.getUsername(); // email
-    }
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@GetMapping("/me")
+	public String retrieveMe(@AuthenticationPrincipal UserDetails selfUser) {
+		return selfUser.getUsername(); // email
+	}
+
+	/**
+	 * Deletes the current authenticated user
+	 *
+	 * @return
+	 */
+	@DeleteMapping("/me")
+	public ResponseEntity<Void> deleteMe(@AuthenticationPrincipal UserDetails selfUser) {
+		User user = userRepository.findByEmail(selfUser.getUsername())
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"User entry not found"));
+
+        userRepository.delete(user);
+
+		return ResponseEntity.noContent().build();
+	}
 }
