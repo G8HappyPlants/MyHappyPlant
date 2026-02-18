1e169975de748b4a25ca9d4ed2fa2a325638eb41
VenomiXeD <crafterious@gmail.com>
2026-02-18 18:49:15 +0100

    Refine and introduce consinstency in UserController utilizing UserService instead. Introduce informal, developer-tools for testing HTTP requests towards each RESt endpoint

--- backend/app/src/main/java/com/example/myhappyplants/controller/UserController.java
+++ backend/app/src/main/java/com/example/myhappyplants/controller/UserController.java
@@ -1,25 +1,24 @@
 package com.example.myhappyplants.controller;
 
-import com.example.myhappyplants.entity.User;
 import com.example.myhappyplants.repository.UserRepository;
-import org.springframework.http.HttpStatus;
+import com.example.myhappyplants.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-	private final UserRepository userRepository;
+	private final UserService userService;
 
-	public UserController(UserRepository userRepository) {
-		this.userRepository = userRepository;
+	public UserController(UserService userService) {
+		this.userService = userService;
 	}
 
 	@GetMapping("/me")
@@ -31,16 +30,12 @@ public class UserController {
 	/**
 	 * Deletes the current authenticated user
 	 *
-	 * @return
+	 * @return {@code HTTP 204} on successful delete
 	 */
 	@DeleteMapping("/me")
 	@PreAuthorize("isAuthenticated()")
 	public ResponseEntity<Void> deleteMe(@AuthenticationPrincipal UserDetails selfUser) {
-		User user = userRepository.findByEmail(selfUser.getUsername())
-                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"User entry not found"));
-
-        userRepository.delete(user);
-
+		userService.removeAccountByUserDetails(selfUser);
 		return ResponseEntity.noContent().build();
 	}
 }
