82346998de14f8938ffa622edf27655699e7147a
David Lexe <David_Lexe@hotmail.com>
2026-02-20 20:47:21 +0100

    /api
    /api

--- backend/app/src/main/java/com/example/myhappyplants/controller/SpeciesController.java
+++ backend/app/src/main/java/com/example/myhappyplants/controller/SpeciesController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/species")
+@RequestMapping("/api/species")
 public class SpeciesController {
     private final SpeciesService speciesService;
 
--- backend/app/src/main/java/com/example/myhappyplants/controller/UserPlantsController.java
+++ backend/app/src/main/java/com/example/myhappyplants/controller/UserPlantsController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/owned")
+@RequestMapping("/api/owned")
 public class UserPlantsController {
     private final UserPlantsService userPlantsService;
 
