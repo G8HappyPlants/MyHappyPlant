9a5f97a0142c5e419c84d9dc60b73819eff458a1
TF139 <torun.flink@gmail.com>
2026-02-27 14:07:10 +0100

    login, logout, register test

--- backend/app/src/test/java/com/example/myhappyplants/AuthTest/AuthService.java
+++ backend/app/src/test/java/com/example/myhappyplants/AuthTest/AuthService.java
@@ -4,11 +4,7 @@ import com.example.myhappyplants.dto.LoginRequest;
 import com.example.myhappyplants.dto.LogoutRequest;
 import com.example.myhappyplants.dto.RegisterRequest;
 
-<<<<<<< HEAD
 interface AuthService {
-=======
-interface AuthService<DeleteAccountRequest> {
->>>>>>> login-test
     AuthResponse login(LoginRequest loginRequest);
     void logout(LogoutRequest logoutRequest);
     AuthResponse register(RegisterRequest registerRequest);
