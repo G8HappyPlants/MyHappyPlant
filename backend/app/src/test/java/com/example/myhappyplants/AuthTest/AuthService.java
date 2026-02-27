65068dfd669ab92f5410666f22e66b8006168c2e
TF139 <torun.flink@gmail.com>
2026-02-27 13:40:32 +0100

    endast logintester

--- backend/app/src/test/java/com/example/myhappyplants/AuthTest/AuthService.java
+++ backend/app/src/test/java/com/example/myhappyplants/AuthTest/AuthService.java
@@ -1,6 +1,10 @@
 package com.example.myhappyplants.AuthTest;
 
-interface AuthService {
+import com.example.myhappyplants.dto.LoginRequest;
+import com.example.myhappyplants.dto.LogoutRequest;
+import com.example.myhappyplants.dto.RegisterRequest;
+
+interface AuthService<DeleteAccountRequest> {
     AuthResponse login(LoginRequest loginRequest);
     void logout(LogoutRequest logoutRequest);
     void deleteAccount(DeleteAccountRequest deleteAccountRequest);
--- backend/app/src/test/java/com/example/myhappyplants/AuthTest/LoginRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.myhappyplants.AuthTest;
-
-public class LoginRequest {
-
-    public LoginRequest(String email, String password){
-
-    }
-}
