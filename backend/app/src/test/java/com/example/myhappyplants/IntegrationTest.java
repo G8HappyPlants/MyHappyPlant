f1fb9cece8e9a7bba9e0d71f353abb2c10099b61
VenomiZeD <crafterious@gmail.com>
2026-03-16 11:39:42 +0100

    Update TF-03-F-6 to match the testcase as described in document
    Streamline integration test base class more for easier creation of tests

--- backend/app/src/test/java/com/example/myhappyplants/IntegrationTest.java
+++ backend/app/src/test/java/com/example/myhappyplants/IntegrationTest.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,5 +21,11 @@ public abstract class IntegrationTest {
 
     protected String json(Object obj) throws Exception {
         return objectMapper.writeValueAsString(obj);
+    }
+
+    protected MockHttpServletRequestBuilder postJson(String urlTemplate, Object content) throws Exception {
+        return MockMvcRequestBuilders.post(urlTemplate)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json(content));
     }
 }
--- backend/app/src/test/java/com/example/myhappyplants/SessionControllerIntegrationTest.java
+++ backend/app/src/test/java/com/example/myhappyplants/SessionControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.example.myhappyplants;
 
 import com.example.myhappyplants.dto.RegisterRequest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,19 +18,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class SessionControllerIntegrationTest extends IntegrationTest {
-
-    @Autowired
-    private MockMvc mockMvc;
+    private static final String MIN_REQ_PASSWORD = "aBcDe12!";
 
     @Test
-    void shouldReturnUser() throws Exception {
-        RegisterRequest request = new RegisterRequest("test123","test@gmail.com","");
+    @DisplayName("TF-03-F-6")
+    void test_register_minimal_request() throws Exception {
+        RegisterRequest request = new RegisterRequest("John Doe","test@gmail.com", MIN_REQ_PASSWORD);
 
         mockMvc.perform(
-                post("/api/auth/register")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json(request))
-                )
-                .andExpect(status().isBadRequest());
+                postJson("/api/auth/register", request))
+                .andExpect(status().isOk()
+                );
     }
 }
